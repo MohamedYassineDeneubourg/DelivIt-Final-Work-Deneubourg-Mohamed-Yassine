210599b80482d564395fe7465171cecea5c2d51a
Mohamed Yassine Deneubourg <mohamed.deneubourg@student.ehb.be>
2020-01-20 22:22:22 +0100

    PhoneSMS

--- Delivit-App/delivit/lib/main.dart
+++ Delivit-App/delivit/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:delivit/colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:international_phone_input/international_phone_input.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,17 +35,156 @@ class _MyHomePageState extends State<MyHomePage> {
   String phoneIsoCode;
   String confirmedNumber;
   Color buttonColor = GrijsDark;
+  String phoneNo;
+  String smsOTP;
+  String verificationId;
+  String errorMessage = '';
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> verifyPhone() async {
+    final PhoneCodeSent smsOTPSent = (String verId, [int forceCodeResend]) {
+      this.verificationId = verId;
+      smsOTPDialog(context).then((value) {
+        print('sign in');
+      });
+    };
+    try {
+      await _auth.verifyPhoneNumber(
+          phoneNumber: this.phoneNo, // PHONE NUMBER TO SEND OTP
+          codeAutoRetrievalTimeout: (String verId) {
+            //Starts the phone number verification process for the given phone number.
+            //Either sends an SMS with a 6 digit code to the phone number specified, or sign's the user in and [verificationCompleted] is called.
+            this.verificationId = verId;
+          },
+          codeSent:
+              smsOTPSent, // WHEN CODE SENT THEN WE OPEN DIALOG TO ENTER OTP.
+          timeout: const Duration(seconds: 20),
+          verificationCompleted: (AuthCredential phoneAuthCredential) {
+            print(phoneAuthCredential);
+          },
+          verificationFailed: (AuthException exceptio) {
+            print('${exceptio.message}');
+          });
+    } catch (e) {
+      handleError(e);
+    }
+  }
+
+  Future<bool> smsOTPDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+            title: Text('VOEG JE SMS-CODE',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            content: Container(
+              height: 85,
+              child: Column(children: [
+                TextField(
+                  maxLength: 6,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25),
+                  onChanged: (value) {
+                    this.smsOTP = value;
+                  },
+                ),
+                (errorMessage != ''
+                    ? Flexible(
+                        child: Text(
+                        errorMessage,
+                        style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 8,
+                            fontWeight: FontWeight.bold),
+                      ))
+                    : Container())
+              ]),
+            ),
+            contentPadding: EdgeInsets.all(20),
+            actions: <Widget>[
+              ButtonTheme(
+                  minWidth: 400.0,
+                  child: RaisedButton(
+                    color: Geel,
+                    child: Text(
+                      "BEVESTIG CODE",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      _auth.currentUser().then((user) {
+                        print("clickkk!");
+                        if (user != null) {
+                          print(user.email);
+                          print(user.getIdToken());
+                          print("Connected!");
+                          Navigator.of(context).pop();
+                          //  Navigator.of(context).pushReplacementNamed('/homepage');
+                        } else {
+                          signIn();
+                        }
+                      });
+                    },
+                  ))
+            ],
+          );
+        });
+  }
+
+  signIn() async {
+    try {
+      print("yeah!");
+      final AuthCredential credential = PhoneAuthProvider.getCredential(
+        verificationId: verificationId,
+        smsCode: smsOTP,
+      );
+      final FirebaseUser user =
+          (await _auth.signInWithCredential(credential)).user;
+      final FirebaseUser currentUser = await _auth.currentUser();
+      assert(user.uid == currentUser.uid);
+      Navigator.of(context).pop();
+
+      //   Navigator.of(context).pushReplacementNamed('/homepage');
+    } catch (e) {
+      handleError(e);
+    }
+  }
+
+  handleError(PlatformException error) {
+    print(error);
+    switch (error.code) {
+      case 'ERROR_INVALID_VERIFICATION_CODE':
+        FocusScope.of(context).requestFocus(new FocusNode());
+        setState(() {
+          errorMessage = 'De code is niet correct..';
+        });
+        Navigator.of(context).pop();
+        smsOTPDialog(context).then((value) {
+          print('sign in');
+        });
+        break;
+      default:
+        setState(() {
+          errorMessage = error.message;
+        });
+
+        break;
+    }
+  }
 
   void onPhoneNumberChange(
       String number, String internationalizedPhoneNumber, String isoCode) {
-        buttonColor = Geel;
+    buttonColor = Geel;
     setState(() {
       phoneNumber = number;
       phoneIsoCode = isoCode;
+      if (phoneIsoCode == "BE") {
+        phoneNo = "+32" + phoneNumber;
+      }
     });
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -104,16 +244,25 @@ class _MyHomePageState extends State<MyHomePage> {
                       padding: EdgeInsets.only(right: 10, left: 10),
                       child: Row(
                         children: <Widget>[
-                          Expanded(child: 
-                          InternationalPhoneInput(
-                              hintText: "Bv. 486 65 53 74",
-                              errorText: "Foute gsm-nummer..",
-                              onPhoneNumberChange: onPhoneNumberChange,
-                              initialPhoneNumber: phoneNumber,
-                              initialSelection: phoneIsoCode),),
+                          Expanded(
+                            child: InternationalPhoneInput(
+                                hintText: "Bv. 486 65 53 74",
+                                errorText: "Foute gsm-nummer..",
+                                onPhoneNumberChange: onPhoneNumberChange,
+                                initialPhoneNumber: phoneNumber,
+                                initialSelection: "BE"),
+                          ),
                           IconButton(
-                            icon: Icon(Icons.send,color: buttonColor,),
-                            onPressed: () {},
+                            icon: Icon(
+                              Icons.send,
+                              color: buttonColor,
+                            ),
+                            onPressed: () {
+                              if (phoneIsoCode == "BE") {
+                                verifyPhone();
+                                print("+32" + phoneNumber);
+                              }
+                            },
                           )
                         ],
                       ))),
